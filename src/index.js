59895570aea8f4c3f4fa40aadb75267b9778248f
YouthDream0925 <youthdream0925@gmail.com>
2024-07-07 02:43:43 -0500

    Added zoom, panning function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import "./styles.css";
 import { fabric } from "fabric";
 
 // initialize fabric canvas and assign to global windows object for debug
+const button = document.getElementById('btn-panning');
 const canvas = (window._canvas = new fabric.Canvas("c", { preserveObjectStacking: true }));
-canvas.setWidth(window.innerWidth);
-canvas.setHeight(window.innerHeight);
-canvas.renderAll();
+
+let startPanning = false;
+let isPanning = false;
+let lastPosX = 0;
+let lastPosY = 0;
 
 const sides = ["left", "right", "centerX", "top", "bottom", "centerY"];
 
 const bindEvents = () => {
     canvas.on('mouse:up', onMouseUp);
+    canvas.on('mouse:down', onMouseDown);
+    canvas.on('mouse:move', onMouseMove);
+    canvas.on('mouse:wheel', onMouseWheel);
+    canvas.on('zoom:changed', onZoomChanged);
     canvas.on('object:moving', onObjectMoving);
     canvas.on('selection:cleared', onSelectionCleared);
 }
@@ -42,37 +49,41 @@ const createRect = (left, top, color, width, height) => {
     canvas.renderAll();
 }
 
-const onScreenResize = () => {
-    canvas.setHeight(window.innerHeight);
-    canvas.setWidth(window.innerWidth);
-    canvas.renderAll();
-}
-
 const preventBrowserZoom = (event) => {
     if (event.ctrlKey) {
         event.preventDefault();
     }
 }
 
+const handleButtonClick = () => {
+    if (isPanning == true) {
+        isPanning = false;
+        button.innerHTML = 'Start';
+    } else {
+        isPanning = true;
+        button.innerHTML = 'End';
+    }
+}
+
 const init = () => {
     bindEvents();
-    createRect(200, 200, "rgba(0, 0, 255, 1)", getRandomNumber(500, 1000), getRandomNumber(500, 1000));
+    createRect(200, 200, "rgba(0, 0, 255, 1)", getRandomNumber(100, 200), getRandomNumber(100, 200));
     createRect(
         Math.floor(Math.random() * canvas.width),
         Math.floor(Math.random() * canvas.height),
         "rgba(0, 255, 0, 1)",
-        getRandomNumber(500, 1000),
-        getRandomNumber(500, 1000)
+        getRandomNumber(100, 200),
+        getRandomNumber(100, 200)
     );
     createRect(
         Math.floor(Math.random() * canvas.width),
         Math.floor(Math.random() * canvas.height),
         "rgba(255, 0, 0, 1)",
-        getRandomNumber(500, 1000),
-        getRandomNumber(500, 1000)
+        getRandomNumber(100, 200),
+        getRandomNumber(100, 200)
     );
 
-    window.addEventListener('resize', onScreenResize)
+    button.addEventListener('click', handleButtonClick);
     window.addEventListener('wheel', preventBrowserZoom, { passive: false });
 }
 
@@ -98,8 +109,78 @@ const clearCanvas = () => {
     canvas.renderAll();
 }
 
-const onMouseUp = (e) => {
+const onMouseDown = (opt) => {
+    if (opt?.button == 1 && isPanning == true) {
+        startPanning = true;
+        lastPosX = opt?.e?.clientX;
+        lastPosY = opt?.e?.clientY;
+    }
+}
+
+const onMouseMove = (opt) => {
+    if (startPanning) {
+        const currentViewportTransform = canvas.viewportTransform;
+        if (currentViewportTransform) {
+            currentViewportTransform[4] += opt?.e?.clientX - lastPosX;
+            currentViewportTransform[5] += opt?.e?.clientY - lastPosY;
+            canvas.requestRenderAll();
+
+            lastPosX = opt?.e?.clientX;
+            lastPosY  = opt?.e?.clientY;
+        }
+    }
+}
+
+const onMouseUp = (opt) => {
+    console.log('onMouseUp', opt)
+
+    startPanning = false;
     clearCanvas();
+}
+
+const onZoomChanged = (opt) => {
+    console.log('xxxxx onZoomChanged xxxxxx', opt);
+
+    const lineProps = {
+        stroke: 'black',
+        strokeWidth: 1 // Set the desired stroke width here
+    }
+
+    canvas.getObjects('line').forEach((line) => {
+        line.set('strokeWidth', lineProps.strokeWidth / canvas.getZoom());
+    });
+
+    canvas.renderAll();
+}
+
+const onMouseWheel = (opt) => {
+    if (opt.e.ctrlKey) {
+        const delta = opt.e.deltaY;
+        let zoom = canvas.getZoom();
+        zoom *= 0.999 ** delta;
+        if (zoom > 30) zoom = 30;
+        if (zoom < 0.3) zoom = 0.3;
+
+        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
+    } else {
+        const e = opt.e;
+        const currentViewportTransform = canvas.viewportTransform;
+        currentViewportTransform[4] -= e.deltaX;
+        currentViewportTransform[5] -= e.deltaY;
+
+        // This is very important part because after panning, if we didn't do it, object will lost their position.
+        canvas.forEachObject(function (obj) {
+            obj.setCoords();
+        });
+
+        canvas.requestRenderAll();
+
+        opt.e.preventDefault();
+        opt.e.stopPropagation();
+    }
 }
 
 const onSelectionCleared = (e) => {
@@ -113,6 +194,8 @@ const onObjectMoving = (e) => {
 
     obj.set("left", Math.round(obj.left));
     obj.set("top", Math.round(obj.top));
+
+    if (isPanning) return;
 
     snapObject(obj);
     drawGuides(obj);
@@ -255,14 +338,24 @@ const drawGuides = (obj) => {
         let ln;
         const lineProps = {
             evented: true,
-            stroke: 'red',
+            stroke: 'black',
+            strokeWidth: 1,
             selectable: false,
             opacity: 1
         };
 
         let marks = [];
 
-        const markSize = 5;
+        const MIN_SIZE = 5; // Maximum size for marks
+        const MIN_ZOOM = 0.3; // Minimum zoom level (10% of the original size)
+        const MAX_ZOOM = 30;  // Maximum zoom level (1000% of the original size)
+        
+        const zoom = canvas.getZoom();
+        const limitedZoomNumber = Math.max(MIN_ZOOM, Math.min(MAX_ZOOM, zoom));
+        const markSize = Math.max(MIN_SIZE, Math.round(MIN_SIZE / limitedZoomNumber));
+
+        console.log('markSize: ', markSize)
+        console.log('limitedZoomNumber: ', limitedZoomNumber)
 
         switch (side) {
             case "top":
@@ -271,12 +364,25 @@ const drawGuides = (obj) => {
                 ln = new fabric.Line(
                     [sortedPointArray[0], value, sortedPointArray[sortedPointArray.length - 1], value],
                     Object.assign(lineProps, {
-                        stroke: 'black'
+                        stroke: 'black',
+                        strokeWidth: lineProps.strokeWidth / zoom
                     })
                 );
+
                 sortedPointArray.forEach(point => {
-                    marks.push(new fabric.Line([point - markSize, value - markSize, point + markSize, value + markSize],Object.assign(lineProps, { stroke: 'black' })));
-                    marks.push(new fabric.Line([point - markSize, value + markSize, point + markSize, value - markSize],Object.assign(lineProps, { stroke: 'black' })));
+                    marks.push(new fabric.Line(
+                        [point - markSize, value - markSize, point + markSize, value + markSize],
+                        Object.assign(lineProps, { 
+                            stroke: 'black'
+                        })
+                    ));
+                    
+                    marks.push(new fabric.Line(
+                        [point - markSize, value + markSize, point + markSize, value - markSize],
+                        Object.assign(lineProps, {
+                            stroke: 'black'
+                        })
+                    ));
                 });  
                 break;
             case "left":
@@ -285,12 +391,25 @@ const drawGuides = (obj) => {
                 ln = new fabric.Line(
                     [value, sortedPointArray[0], value, sortedPointArray[sortedPointArray.length - 1]],
                     Object.assign(lineProps, {
-                        stroke: 'black'
+                        stroke: 'black',
+                        strokeWidth: lineProps.strokeWidth / zoom
                     })
                 );
+
                 sortedPointArray.forEach(point => {
-                    marks.push(new fabric.Line([value - markSize, point - markSize, value + markSize, point + markSize],Object.assign(lineProps, { stroke: 'black' })));
-                    marks.push(new fabric.Line([value - markSize, point + markSize, value + markSize, point - markSize],Object.assign(lineProps, { stroke: 'black' })));
+                    marks.push(new fabric.Line(
+                        [value - markSize, point - markSize, value + markSize, point + markSize],
+                        Object.assign(lineProps, { 
+                            stroke: 'black' 
+                        })
+                    ));
+
+                    marks.push(new fabric.Line(
+                        [value - markSize, point + markSize, value + markSize, point - markSize],
+                        Object.assign(lineProps, { 
+                            stroke: 'black'
+                        })
+                    ));
                 });
                 break;
         }
